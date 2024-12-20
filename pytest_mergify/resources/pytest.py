7f5872e583c1e0def0299cd6418eff6103109f25
Julien Danjou <julien@danjou.info>
2024-12-20 14:46:12 +0100

    Update pytest_mergify/resources/pytest.py
    Co-authored-by: Charly Laurent <douglas.blackwood@protonmail.com>

--- pytest_mergify/resources/pytest.py
+++ pytest_mergify/resources/pytest.py
@@ -3,7 +3,7 @@ from opentelemetry.sdk.resources import Resource, ResourceDetector
 
 
 class PytestResourceDetector(ResourceDetector):
-    """Detects OpenTelemetry Resource attributes for GitHub Actions."""
+    """Detects OpenTelemetry Resource attributes for Pytest."""
 
     def detect(self) -> Resource:
         return Resource(
