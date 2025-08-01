bdec1925bb6b829e88a5398ea680b4192265ddd3
Mehdi ABAAKOUK <sileht@sileht.net>
2025-08-01 14:42:41 +0200

    feat: add jenkins support (#147)
    Fixes MRGFY-5591

--- pytest_mergify/utils.py
+++ pytest_mergify/utils.py
@@ -2,11 +2,14 @@ import os
 import re
 import typing
 
-CIProviderT = typing.Literal["github_actions", "circleci", "pytest_mergify_suite"]
+CIProviderT = typing.Literal[
+    "github_actions", "circleci", "pytest_mergify_suite", "jenkins"
+]
 
 SUPPORTED_CIs: typing.Dict[str, CIProviderT] = {
     "GITHUB_ACTIONS": "github_actions",
     "CIRCLECI": "circleci",
+    "JENKINS_URL": "jenkins",
     "_PYTEST_MERGIFY_TEST": "pytest_mergify_suite",
 }
 
@@ -19,8 +22,27 @@ def is_in_ci() -> bool:
 
 def get_ci_provider() -> typing.Optional[CIProviderT]:
     for envvar, name in SUPPORTED_CIs.items():
-        if envvar in os.environ and strtobool(os.environ[envvar]):
-            return name
+        if envvar in os.environ:
+            try:
+                enabled = strtobool(os.environ[envvar])
+            except ValueError:
+                # Not a boolean, just check it's not empty
+                enabled = bool(os.environ[envvar].strip())
+            if enabled:
+                return name
+
+    return None
+
+
+def _get_repository_name_from_env_url(env: str) -> typing.Optional[str]:
+    repository_url = os.getenv(env)
+    if repository_url and (
+        match := re.match(
+            r"(https?://[\w.-]+/)?(?P<full_name>[\w.-]+/[\w.-]+)/?$",
+            repository_url,
+        )
+    ):
+        return match.group("full_name")
 
     return None
 
@@ -28,18 +50,14 @@ def get_ci_provider() -> typing.Optional[CIProviderT]:
 def get_repository_name() -> typing.Optional[str]:
     provider = get_ci_provider()
 
+    if provider == "jenkins":
+        return _get_repository_name_from_env_url("GIT_URL")
+
     if provider == "github_actions":
         return os.getenv("GITHUB_REPOSITORY")
 
     if provider == "circleci":
-        repository_url = os.getenv("CIRCLE_REPOSITORY_URL")
-        if repository_url and (
-            match := re.match(
-                r"(https?://[\w.-]+/)?(?P<full_name>[\w.-]+/[\w.-]+)/?$",
-                repository_url,
-            )
-        ):
-            return match.group("full_name")
+        return _get_repository_name_from_env_url("CIRCLE_REPOSITORY_URL")
 
     if provider == "pytest_mergify_suite":
         return "Mergifyio/pytest-mergify"
