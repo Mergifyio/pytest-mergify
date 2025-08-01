66b509abf5f15dd4140ee0f972e85ccf0bdff7b1
Mehdi ABAAKOUK <sileht@sileht.net>
2025-08-01 12:11:18 +0200

    chore: create a common helper for resource attributes mapping
    This avoids repeating the same for loop everywhere.
    
    Change-Id: If1f5abd0af1baf471988f14e9ed2f1ecf1d53033

--- pytest_mergify/resources/github_actions.py
+++ pytest_mergify/resources/github_actions.py
@@ -1,30 +1,33 @@
+import json
 import os
 import pathlib
-import json
 import typing
 
 from opentelemetry.sdk.resources import Resource, ResourceDetector
-
-from opentelemetry.semconv._incubating.attributes import cicd_attributes
-from opentelemetry.semconv._incubating.attributes import vcs_attributes
+from opentelemetry.semconv._incubating.attributes import cicd_attributes, vcs_attributes
 
 from pytest_mergify import utils
 
 
+def _get_github_workflow_job_head_sha() -> typing.Optional[str]:
+    if os.getenv("GITHUB_EVENT_NAME") == "pull_request":
+        # NOTE: for pull request, the job head sha is the pull request head_sha,
+        # not the GITHUB_SHA, which contains the pull request merge_commit_sha
+        event_raw_path = os.getenv("GITHUB_EVENT_PATH")
+        if event_raw_path and ((event_path := pathlib.Path(event_raw_path)).is_file()):
+            event = json.loads(event_path.read_bytes())
+            return str(event["pull_request"]["head"]["sha"])
+    return os.getenv("GITHUB_SHA")
+
+
+def _get_repository_url() -> typing.Optional[str]:
+    if "GITHUB_SERVER_URL" in os.environ and "GITHUB_REPOSITORY" in os.environ:
+        return os.environ["GITHUB_SERVER_URL"] + "/" + os.environ["GITHUB_REPOSITORY"]
+    return None
+
+
 class GitHubActionsResourceDetector(ResourceDetector):
     """Detects OpenTelemetry Resource attributes for GitHub Actions."""
-
-    @staticmethod
-    def get_github_actions_head_sha() -> typing.Optional[str]:
-        if os.getenv("GITHUB_EVENT_NAME") == "pull_request":
-            # NOTE: we want the head sha of the pull request
-            event_raw_path = os.getenv("GITHUB_EVENT_PATH")
-            if event_raw_path and (
-                (event_path := pathlib.Path(event_raw_path)).is_file()
-            ):
-                event = json.loads(event_path.read_bytes())
-                return str(event["pull_request"]["head"]["sha"])
-        return os.getenv("GITHUB_SHA")
 
     OPENTELEMETRY_GHA_MAPPING = {
         cicd_attributes.CICD_PIPELINE_NAME: (str, "GITHUB_WORKFLOW"),
@@ -37,25 +40,13 @@ class GitHubActionsResourceDetector(ResourceDetector):
         vcs_attributes.VCS_REF_BASE_NAME: (str, "GITHUB_BASE_REF"),
         "vcs.repository.name": (str, "GITHUB_REPOSITORY"),
         "vcs.repository.id": (int, "GITHUB_REPOSITORY_ID"),
+        vcs_attributes.VCS_REPOSITORY_URL_FULL: (str, _get_repository_url),
+        vcs_attributes.VCS_REF_HEAD_REVISION: (str, _get_github_workflow_job_head_sha),
     }
 
     def detect(self) -> Resource:
         if utils.get_ci_provider() != "github_actions":
             return Resource({})
 
-        attributes = {}
-
-        if "GITHUB_SERVER_URL" in os.environ and "GITHUB_REPOSITORY" in os.environ:
-            attributes[vcs_attributes.VCS_REPOSITORY_URL_FULL] = (
-                os.environ["GITHUB_SERVER_URL"] + "/" + os.environ["GITHUB_REPOSITORY"]
-            )
-
-        head_sha = self.get_github_actions_head_sha()
-        if head_sha is not None:
-            attributes[vcs_attributes.VCS_REF_HEAD_REVISION] = head_sha
-
-        for attribute_name, (type_, envvar) in self.OPENTELEMETRY_GHA_MAPPING.items():
-            if envvar in os.environ:
-                attributes[attribute_name] = type_(os.environ[envvar])
-
+        attributes = utils.get_attributes(self.OPENTELEMETRY_GHA_MAPPING)
         return Resource(attributes)
