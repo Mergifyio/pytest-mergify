89a9536b3b1c2cedd9b166a0068a1918591bf4f3
Mehdi ABAAKOUK <sileht@sileht.net>
2025-08-05 10:49:02 +0200

    chore: create a common helper for resource attributes mapping (#149)
    This avoids repeating the same for loop everywhere.

--- pytest_mergify/resources/mergify.py
+++ pytest_mergify/resources/mergify.py
@@ -1,14 +1,15 @@
-import os
-
 from opentelemetry.sdk.resources import Resource, ResourceDetector
+
+from pytest_mergify import utils
 
 
 class MergifyResourceDetector(ResourceDetector):
     """Detects OpenTelemetry Resource attributes for Mergify fields."""
 
-    def detect(self) -> Resource:
-        r = {}
-        if "MERGIFY_TEST_JOB_NAME" in os.environ:
-            r["mergify.test.job.name"] = os.environ["MERGIFY_TEST_JOB_NAME"]
+    OPENTELEMETRY_MERGIFY_MAPPING = {
+        "mergify.test.job.name": (str, "MERGIFY_TEST_JOB_NAME"),
+    }
 
-        return Resource(r)
+    def detect(self) -> Resource:
+        attributes = utils.get_attributes(self.OPENTELEMETRY_MERGIFY_MAPPING)
+        return Resource(attributes)
