ced8bcf7a7fbc02cb69e973e1e420e71a00600aa
Mehdi ABAAKOUK <sileht@sileht.net>
2025-08-06 08:01:33 +0200

    fix(jenkins): get git branch correctly (#151)
    Fixes MRGFY-5595

--- pytest_mergify/resources/jenkins.py
+++ pytest_mergify/resources/jenkins.py
@@ -1,7 +1,25 @@
+import os
+import typing
+
 from opentelemetry.sdk.resources import Resource, ResourceDetector
 from opentelemetry.semconv._incubating.attributes import cicd_attributes, vcs_attributes
 
 from pytest_mergify import utils
+
+GIT_BRANCH_PREFIXES = ("origin/", "refs/heads/")
+
+
+def _get_branch() -> typing.Optional[str]:
+    branch = os.getenv("GIT_BRANCH")
+    if branch:
+        # NOTE(sileht): it's not 100% bullet proof but since it's very complicated
+        # and unlikely to change/add a remote with Jenkins Git/GitHub plugins,
+        # we just handle the most common cases.
+        for prefix in GIT_BRANCH_PREFIXES:
+            if branch.startswith(prefix):
+                return branch[len(prefix) :]
+        return branch
+    return None
 
 
 class JenkinsResourceDetector(ResourceDetector):
@@ -13,7 +31,7 @@ class JenkinsResourceDetector(ResourceDetector):
         cicd_attributes.CICD_PIPELINE_RUN_ID: (str, "BUILD_ID"),
         "cicd.pipeline.run.url": (str, "BUILD_URL"),
         "cicd.pipeline.runner.name": (str, "NODE_NAME"),
-        vcs_attributes.VCS_REF_HEAD_NAME: (str, "GIT_BRANCH"),
+        vcs_attributes.VCS_REF_HEAD_NAME: (str, _get_branch),
         vcs_attributes.VCS_REF_HEAD_REVISION: (str, "GIT_COMMIT"),
         vcs_attributes.VCS_REPOSITORY_URL_FULL: (str, "GIT_URL"),
         "vcs.repository.name": (
