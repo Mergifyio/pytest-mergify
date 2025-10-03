a1c57b33e637d4a7bfc2d82d3e4594cdcfdddbe8
Remy DUTHU <remy.duthu@gmail.com>
2025-10-03 16:03:39 +0200

    feat(ci-insights): Compute retry costs dynamically
    Fixes: MRGFY-5826
    Change-Id: I9c00b20b5daafab2de6fa124daa22d6573857f56

--- tests/test_ci_insights.py
+++ tests/test_ci_insights.py
@@ -1,3 +1,4 @@
+import datetime
 import re
 import typing
 
@@ -54,8 +55,8 @@ def test_load_flaky_detection(monkeypatch: pytest.MonkeyPatch) -> None:
     _make_test_names_mock(test_names=["a::test_a", "b::test_b"])
 
     client = _make_test_client()
-    assert not client.flaky_detection_error_message
-    assert client.existing_test_names == ["a::test_a", "b::test_b"]
+    assert not client._flaky_detection_error_message
+    assert client._existing_test_names == ["a::test_a", "b::test_b"]
 
 
 @responses.activate
@@ -66,9 +67,9 @@ def test_load_flaky_detection_error(monkeypatch: pytest.MonkeyPatch) -> None:
     _make_test_names_mock(status=500)
 
     client = _make_test_client()
-    assert not client.existing_test_names
-    assert client.flaky_detection_error_message is not None
-    assert "500 Server Error" in client.flaky_detection_error_message
+    assert not client._existing_test_names
+    assert client._flaky_detection_error_message is not None
+    assert "500 Server Error" in client._flaky_detection_error_message
 
 
 @responses.activate
@@ -109,23 +110,35 @@ def test_flaky_detection(
             pytest.skip("I'm skipped!")
         """
     )
-    result.assert_outcomes(
-        passed=2 + (2 * 5),  # 2 initial runs, 5 retries for each test.
-        failed=1,  # Only the first run of the flaky test.
-        skipped=1,  # Skipped tests are excluded from flaky detection.
-    )
+
+    outcomes = result.parseoutcomes()
+
+    # We can't predict the exact number because it depends on the time it takes
+    # to run the tests. We just want to make sure that the tests are tested
+    # multiple time.
+    assert outcomes["passed"] > 1000
+
+    # Only the first run of the flaky test.
+    assert outcomes["failed"] == 1
+
+    # The skipped test is tested only once because skipped tests are excluded from the flaky detection.
+    assert outcomes["skipped"] == 1
 
     assert re.search(
-        r"""Fetched 2 existing tests
-Detected 2 new tests
-  - test_flaky_detection\.py::test_bar \(\d+ms\)
-  - test_flaky_detection\.py::test_baz \(\d+ms\)""",
+        r"""🐛 Flaky detection
+- Used [0-9.]+ % of the budget \([0-9.]+ s/[0-9.]+ s\)
+- Active for 2 new test\(s\):
+    • 'test_flaky_detection\.py::test_bar' has been tested \d+ times using approx\. [0-9.]+ % of the budget \([0-9.]+ s/[0-9.]+ s\)
+    • 'test_flaky_detection\.py::test_baz' has been tested \d+ times using approx\. [0-9.]+ % of the budget \([0-9.]+ s/[0-9.]+ s\)""",
         result.stdout.str(),
         re.MULTILINE,
     )
 
     assert spans is not None
-    assert len(spans) == 15
+
+    # 1 span for the session and one per test.
+    assert len(spans) == 1 + sum(outcomes.values())
+
     for test_name, expected in {
         "test_flaky_detection.py::test_foo": False,
         "test_flaky_detection.py::test_bar": True,
@@ -138,3 +151,99 @@ Detected 2 new tests
         assert span.attributes is not None
         if expected:
             assert span.attributes.get("cicd.test.new")
+
+
+@pytest.mark.parametrize(
+    argnames=["budget_duration", "test_durations", "expected_allocation"],
+    argvalues=[
+        pytest.param(
+            datetime.timedelta(seconds=30),
+            {
+                "test_a": datetime.timedelta(milliseconds=10),
+                "test_b": datetime.timedelta(milliseconds=20),
+                "test_c": datetime.timedelta(milliseconds=30),
+                "test_d": datetime.timedelta(milliseconds=40),
+            },
+            {
+                "test_a": 750,
+                "test_b": 375,
+                "test_c": 250,
+                "test_d": 187,
+            },
+            id="Basic",
+        ),
+        pytest.param(
+            datetime.timedelta(seconds=10),
+            {
+                "test_a": datetime.timedelta(milliseconds=10),
+                "test_b": datetime.timedelta(milliseconds=2),
+                "test_c": datetime.timedelta(milliseconds=100),
+                "test_d": datetime.timedelta(seconds=10),  # Not affordable.
+                "test_e": datetime.timedelta(milliseconds=1),
+                "test_f": datetime.timedelta(milliseconds=100),
+            },
+            {
+                "test_a": 233,
+                "test_b": ci_insights._MAX_TEST_RETRY_COUNT,
+                "test_c": 23,
+                "test_e": ci_insights._MAX_TEST_RETRY_COUNT,
+                "test_f": 23,
+            },
+            id="With slow and fast tests",
+        ),
+        pytest.param(
+            datetime.timedelta(seconds=10),
+            {},
+            {},
+            id="Empty test durations",
+        ),
+        pytest.param(
+            datetime.timedelta(),
+            {"test_a": datetime.timedelta(milliseconds=1)},
+            {},
+            id="Empty budget duration",
+        ),
+        pytest.param(
+            datetime.timedelta(milliseconds=1),
+            {
+                "test_a": datetime.timedelta(seconds=1),
+                "test_b": datetime.timedelta(seconds=2),
+            },
+            {},
+            id="Budget too small",
+        ),
+        pytest.param(
+            datetime.timedelta(seconds=5),
+            {"test_a": datetime.timedelta(seconds=1)},
+            {"test_a": 5},
+            id="Exact budget for minimum retries",
+        ),
+        pytest.param(
+            datetime.timedelta(seconds=2000),
+            {"test_a": datetime.timedelta(milliseconds=1)},
+            {"test_a": 1000},
+            id="Test reaching maximum retries",
+        ),
+    ],
+)
+def test_allocate_test_retries(
+    budget_duration: datetime.timedelta,
+    test_durations: typing.Dict[str, datetime.timedelta],
+    expected_allocation: typing.Dict[str, int],
+) -> None:
+    allocation = ci_insights._allocate_test_retries(budget_duration, test_durations)
+
+    total_duration = datetime.timedelta()
+
+    for test, retry_count in allocation.items():
+        assert test in test_durations
+        assert retry_count >= ci_insights._MIN_TEST_RETRY_COUNT
+        assert retry_count <= ci_insights._MAX_TEST_RETRY_COUNT
+
+        total_duration += test_durations[test] * retry_count
+
+    # We want to make sure that we are never consuming more than the allocated
+    # budget.
+    assert total_duration <= budget_duration
+
+    assert allocation == expected_allocation
